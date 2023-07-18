7aab2b485e565ff7a17a3c9c745651dcf27ad631
Danila <Bbunok991@yandex.ru>
2023-07-18 19:46:33 +0300

    The version_2

--- Task_56/Program.cs
+++ Task_56/Program.cs
@@ -1,12 +1,10 @@
-﻿﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
-В итоге получается вот такой массив:
-7 4 2 1
-9 5 3 2
-8 4 4 2*/
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
 
 int InPut(string text)
 {
@@ -40,31 +38,6 @@ void PrintArray(int[,] arrayForPrint)
         System.Console.WriteLine();
     }
     System.Console.WriteLine("\n");
-}
-
-void PrintArray2(int[,] arrayForPrint)
-{
-    int last = 0;
-    for(int i = 0; i < arrayForPrint.GetLength(0); i++)
-    {
-        int j = 0;
-        last = arrayForPrint[i, j];
-        for(; j < arrayForPrint.GetLength(1); j++)
-        {
-            if(arrayForPrint[i, j] < last)
-            {
-                last = arrayForPrint[i, j];
-            }
-            else if(arrayForPrint[i, j] > last)
-            {
-                arrayForPrint[i, j - 1] = arrayForPrint[i, j];
-                arrayForPrint[i, j] = last;
-            }
-            else if(arrayForPrint[i, j] == last) continue;
-
-        }
-    }
-    PrintArray(arrayForPrint);
 }
 
 int row = InPut("Enter a row: ");
